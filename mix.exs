f67ebcadae12f73aa2f0262622578f7924138b30
Ilyich Vismara <ilyich@arkemis.it>
2023-06-28 12:17:39 +0200

    chore: set version to v0.2.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ArkePostgres.MixProject do
   def project do
     [
       app: :arke_postgres,
-      version: "0.2.2",
+      version: "0.2.3",
       build_path: "./_build",
       config_path: "./config/config.exs",
       deps_path: "./deps",
