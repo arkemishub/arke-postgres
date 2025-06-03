7659d9d059257e0269eca7c7e33bf26d5fc20103
Erik <e.ferrari@arkemis.it>
2025-06-03 12:40:17 +0200

    v0.4.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ArkePostgres.MixProject do
   use Mix.Project
 
-  @version "0.3.7"
+  @version "0.4.0"
   @scm_url "https://github.com/arkemishub/arke-postgres"
   @site_url "https://arkehub.com"
 
